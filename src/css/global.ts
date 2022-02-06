4c61c69da78515547c5a89bc7e5e7321f343362c
Carlos Pereira <ce.magalhaespereira@gmail.com>
2022-02-06 12:10:07 -0300

    Add global style

--- src/css/global.ts
+++ src/css/global.ts
@@ -3,11 +3,20 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
 :root {
     // colors
+    --primary-moderate-blue: hsl(238, 40%, 52%);
+    --primary-soft-red: hsl(358, 79%, 66%);
+    --primary-light-grayish-blue: hsl(239, 57%, 85%);
+    --primary-pale-red: hsl(357, 100%, 86%);
+
+    --neutral-dark-blue: hsl(212, 24%, 26%);
+    --neutral-grayish-blue: hsl(211, 10%, 45%);
+    --neutral-light-gray: hsl(223, 19%, 93%);
+    --neutral-very-light-gray: hsl(228, 33%, 97%);
+    --neutral-white: hsl(0, 0%, 100%);
 
     // layout
     --mobile: 480px;
     --desktop: 1440px;
-    // border
 }
 * {
     margin: 0;
